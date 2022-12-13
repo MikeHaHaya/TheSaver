42e1ffc16a585640a34639789294820fe031b297
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2022-12-13 13:12:55 +0200

    Initial commit

---
+++ src/main/java/app/core/thesaver/TheSaverApplication.java
@@ -0,0 +1,13 @@
+package app.core.thesaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheSaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheSaverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/core/thesaver/TheSaverApplicationTests.java
@@ -0,0 +1,13 @@
+package app.core.thesaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheSaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
