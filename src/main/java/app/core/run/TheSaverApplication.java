3537aceae049282d62c69e5e934fce1f866b3d69
Daniel Har-Oz <daniel.haroz2001@gmail.com>
2022-12-13 13:35:32 +0200

    Basic structure - may change later

--- src/main/java/app/core/run/TheSaverApplication.java
+++ src/main/java/app/core/run/TheSaverApplication.java
@@ -1,4 +1,4 @@
-package app.core.thesaver;
+package app.core.run;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TheSaverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TheSaverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TheSaverApplication.class, args);
+    }
 
 }
--- src/test/java/app/core/run/TheSaverApplicationTests.java
+++ src/test/java/app/core/run/TheSaverApplicationTests.java
@@ -1,4 +1,4 @@
-package app.core.thesaver;
+package app.core.run;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TheSaverApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
